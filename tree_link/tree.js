561e754c63b250797064256b1451f3461ab6f561
Rangkynsai Saio <47221516+Rangkynsai@users.noreply.github.com>
2020-01-22 23:14:45 +0530

    Version_2_using_Javascript
    The anchor tag of a button has replaced by a Javascript function

---
+++ tree_link/tree.js
@@ -0,0 +1,12 @@
+function myfunction1(){
+    window.location.href = 'https://www.linkedin.com/in/rangkynsai-saio-264028196/';
+}
+function myfunction2(){
+    window.location.href = "https://www.facebook.com/kynsai.saio";
+}
+function myfunction3(){
+    window.location.href = "https://www.instagram.com/co_dtechr/";
+}
+function myfunction4(){
+    window.location.href = "https://github.com/Rangkynsai";
+}
